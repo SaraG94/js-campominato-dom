129c5b4ccc57dde2e4fdcdc5102fbdbe3b90c97e
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-13 20:51:13 +0100

    FIX style

--- js/app.js
+++ js/app.js
@@ -1,26 +1,27 @@
-console.log('ciaoo');
 //prendere elemento risultato
 const resultElement = document.getElementById('risultato');
 
 //prendere il tasto Gioca
 const buttonElement = document.getElementById('pulsante');
-console.log(buttonElement);
+
+const punti = document.getElementById('numbero-punteggio');
+
+
 
 //al click su play si genera griglia
 buttonElement.addEventListener('click', startGame)
-    console.log('click');
-    
-//array numero random bombe
+console.log('click');
+
+let celleElements
+
+//definire lato
+let latoGriglia = 10;
+console.log(latoGriglia);
+
 let bombs = [];
 
-//generare 16 numeri random 
-while(bombs.length < 16){
-    let numbBombs = Math.floor(Math.random() * 100) + 1;
-    console.log(numbBombs);
-
-    bombs.push(numbBombs)
-}
-console.log(bombs)
+let punteggio = 0;
+    
 
 function startGame (){
     //dichiarare griglia vuota
@@ -32,18 +33,15 @@ function startGame (){
 
     grigliaElement.innerHTML = '';
     
-    //definire lato e celle
-    let latoGriglia = 10;
-    console.log(latoGriglia);
-
+    //definire celle
     let numeroCelle = latoGriglia * latoGriglia;
-    console.log(numeroCelle);
 
     let divElement = '';
 
+    bombs = generaBombe (16, 1, numeroCelle)
+
     for (let i = 0; i < numeroCelle; i++) {
         let num = i + 1
-        console.log(num)
 
         divElement = document.createElement('div')
         
@@ -62,7 +60,7 @@ function startGame (){
     }
 
     //prendere celle
-    const celleElements = document.querySelectorAll('.celle');
+    celleElements = document.querySelectorAll('.celle');
     console.log(celleElements);
 
     for (let i = 0; i < celleElements.length; i++) {
@@ -76,6 +74,39 @@ function startGame (){
     return 'Fine star game'
 }
 
+//generare 16 numeri random 
+function generaBombe(numeroBombe, min, max){
+    //array numero random bombe
+    let bombs = [];
+
+    while(bombs.length < numeroBombe){
+        let numbBombs = Math.floor(Math.random() * max) + 1;
+        console.log(numbBombs);
+    
+        if(bombs.includes(numbBombs)===false){
+            bombs.push(numbBombs);
+        }
+    }
+    return bombs;
+}
+
+function gameOver(){
+    //rimuovere eventListener da celle 
+    for(let i = 0; i< celleElements.length; i++){
+        const cella = celleElements[i];
+
+        //confrontro celle e array
+        let cellaBombs = bombs.includes(parseInt(cella.innerHTML));
+        console.log(cellaBombs, 'boom');
+
+        if(cellaBombs === true){
+            // bg-color red su cella
+            cella.classList.add('boom');        
+            resultElement.innerHTML='Hai perso!'
+        }
+        cella.removeEventListener('click', onClick);
+    }
+}
 
 function onClick(event){
     console.log(event);
@@ -83,6 +114,7 @@ function onClick(event){
 	console.log(this);
 
     const cella = this;
+    console.log(cella)
 	console.log(cella.innerHTML);
 
     //confrontro celle e array
@@ -91,17 +123,26 @@ function onClick(event){
 
     //se clicco su cella con bomba
     if(cellaBombs === true){
+        
+        gameOver()
         // bg-color red su cella
         cella.classList.add('boom');        
-	    cella.removeEventListener('click', onClick);
         resultElement.innerHTML='Hai perso!'
     } else{
+        // controllo se utente vince
+		let numeroCaselleSenzaBombe = latoGriglia * latoGriglia - bombs.length;
+		if (punteggio === numeroCaselleSenzaBombe) {
+			console.log('hai vinto');
+		}
 
         cella.classList.add('new-color');
-	    cella.removeEventListener('click', onClick);
+
+        punteggio++  ;        
+        console.log(punteggio); 
+        punti.innerHTML = punteggio;  
+		
+        cella.removeEventListener('click', onClick);
     }
-    
-    
 }
    
    
